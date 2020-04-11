28e5652006f2b1a417186df09dcc746855aed5ec
emryswarlock <ardha275@gmail.com>
2020-04-12 00:49:19 +0700

    Updates

---
+++ precache-manifest.af4ae4a400fe5a0fda86b1f9c2d28923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fe371a7c763e68d0714aab8a3f354e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad6873364d5e5d4dcd92",
+    "url": "/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "a24e6aa63f06372d4860",
+    "url": "/static/css/main.370f4b20.chunk.css"
+  },
+  {
+    "revision": "ad6873364d5e5d4dcd92",
+    "url": "/static/js/2.608d8ec9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.608d8ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24e6aa63f06372d4860",
+    "url": "/static/js/main.1719d578.chunk.js"
+  },
+  {
+    "revision": "82b5cc9976bd34d07837",
+    "url": "/static/js/runtime-main.ed475fe9.js"
+  }
+]);
